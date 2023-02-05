a4cc187ed52726e4bf8cec26a0e2c96d6c10cf69
Faizan <faizansohail077@gmail.com>
2023-02-06 00:03:49 +0500

    added navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
+"use client"
+import { Navbar } from '@/components'
 import './globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function RootLayout({
   children,
@@ -12,7 +15,14 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
-    </html>
+      <body>
+        <ChakraProvider>
+
+          <Navbar />
+          {children}
+          123
+        </ChakraProvider>
+      </body>
+    </html >
   )
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,178 @@
+import {
+    Box,
+    Flex,
+    Text,
+    IconButton,
+    Button,
+    Stack,
+    Collapse,
+    Link,
+    Popover,
+    PopoverTrigger,
+    useColorModeValue,
+    useBreakpointValue,
+    useDisclosure,
+} from '@chakra-ui/react';
+import {
+    HamburgerIcon,
+    CloseIcon,
+} from '@chakra-ui/icons';
+import Image from 'next/image';
+
+export default function WithSubnavigation() {
+    const { isOpen, onToggle } = useDisclosure();
+
+    return (
+        <Box>
+            <Flex
+                bg={useColorModeValue('white', 'gray.800')}
+                color={useColorModeValue('gray.600', 'white')}
+                minH={'60px'}
+                py={{ base: 2 }}
+                px={{ base: 4 }}
+                borderBottom={1}
+                borderStyle={'solid'}
+                borderColor={useColorModeValue('gray.200', 'gray.900')}
+                align={'center'}>
+                <Flex
+                    flex={{ base: 1, md: 'auto' }}
+                    ml={{ base: -2 }}
+                    display={{ base: 'flex', md: 'none' }}>
+                    <IconButton
+                        onClick={onToggle}
+                        icon={
+                            isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+                        }
+                        variant={'ghost'}
+                        aria-label={'Toggle Navigation'}
+                    />
+                </Flex>
+                <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+                    <Text
+                        textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+                        fontFamily={'heading'}
+                        color={useColorModeValue('gray.800', 'white')}>
+                        <Image src={require('@/assests/panaverse-logo.png')} alt={'logo'} height={100} width={100} />
+                    </Text>
+
+                    <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+                        <DesktopNav />
+                    </Flex>
+                </Flex>
+
+                <Stack
+                    flex={{ base: 1, md: 0 }}
+                    justify={'flex-end'}
+                    direction={'row'}
+                    spacing={6}>
+                    <Button
+                        as={'a'}
+                        display={{ base: 'inline-flex' }}
+                        fontSize={'sm'}
+                        fontWeight={600}
+                        color={'white'}
+                        bg={'pink.400'}
+                        href={'#'}
+                        _hover={{
+                            bg: 'pink.300',
+                        }}>
+                        Apply
+                    </Button>
+                </Stack>
+            </Flex>
+
+            <Collapse in={isOpen} animateOpacity>
+                <MobileNav />
+            </Collapse>
+        </Box>
+    );
+}
+
+const DesktopNav = () => {
+    const linkColor = useColorModeValue('gray.600', 'gray.200');
+    const linkHoverColor = useColorModeValue('gray.800', 'white');
+
+    return (
+        <Stack direction={'row'} spacing={4}>
+            {NAV_ITEMS.map((navItem) => (
+                <Box key={navItem.label}>
+                    <Popover trigger={'hover'} placement={'bottom-start'}>
+                        <PopoverTrigger>
+                            <Link
+                                p={2}
+                                href={navItem.href ?? '#'}
+                                fontSize={'sm'}
+                                fontWeight={500}
+                                color={linkColor}
+                                _hover={{
+                                    textDecoration: 'none',
+                                    color: linkHoverColor,
+                                }}>
+                                <Flex>
+
+                                    {navItem.label}
+                                </Flex>
+                            </Link>
+                        </PopoverTrigger>
+
+
+                    </Popover>
+                </Box>
+            ))}
+        </Stack>
+    );
+};
+
+const MobileNav = () => {
+    return (
+        <Stack
+            bg={useColorModeValue('white', 'gray.800')}
+            p={4}
+            display={{ md: 'none' }}>
+            {NAV_ITEMS.map((navItem) => (
+                <MobileNavItem key={navItem.label} {...navItem} />
+            ))}
+        </Stack>
+    );
+};
+
+const MobileNavItem = ({ label, href }: NavItem) => {
+    return (
+        <Stack spacing={4}>
+            <Flex
+                py={2}
+                as={Link}
+                href={href ?? '#'}
+                justify={'space-between'}
+                align={'center'}
+                _hover={{
+                    textDecoration: 'none',
+                }}>
+                <Text
+                    fontWeight={600}
+                    color={useColorModeValue('gray.600', 'gray.200')}>
+                    {label}
+                </Text>
+            </Flex>
+
+
+        </Stack>
+    );
+};
+
+interface NavItem {
+    label: string;
+    subLabel?: string;
+    href?: string;
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+    {
+        label: 'Home',
+        href: '#',
+    },
+    {
+        label: 'Courses',
+        href: '#',
+    },
+];
